b5428b29b4febbd50b8d7a9513262dd9ed9dab67
Priyanshu Jain <priyanshu0405jain@gmail.com>
2021-10-26 20:45:39 +0530

    website name changed

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
-    <title>CVIP 2020</title>
+    <title>UPCON 2022</title>
     <?php include("layout/meta.php");?>
 </head>
 
