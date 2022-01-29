f241cb70aee1d3d149190bf8d569cbf775b91a01
Priyanshu Jain <priyanshu0405jain@gmail.com>
2022-01-29 23:19:05 +0530

    issues fixed

--- layout/home_display_pane.php
+++ layout/home_display_pane.php
@@ -7,7 +7,7 @@
                 <h1 class="mb-1 font-weight-bold text-uppercase"
                     data-scrollax="properties: { translateY: '30%', opacity: 1.6 }" style=" color: maroon;">9<sup
                         class="text-lowercase">th</sup> IEEE UP Section Conference on <br><span
-                        style="font-size: 50px;">Electrical, Computer and Electronics</span></h1>
+                        style="font-size: 50px;">Electrical, Electronics and Computer Engineering</span></h1>
                 <p class="mb-1"
                     style="color:white;display: inline-block;height: 32px;padding-left: 22px;padding-right: 24px;background: #7f66ff;align-items: center;justify-content: flex-start"
                     data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span
